38ea4af6f3ca381e65a0118f75e9e67d26afc3ff
takoua1 <samaali.takoua@gmail.com>
2024-11-23 17:07:46 +0100

    Modified scripts/start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Attendre que la base de données soit prête sur db:5432 avec un timeout de 30 secondes
+# Vérifier si le fichier JAR existe
+if [ ! -f "/forumSocialX.jar" ]; then
+  echo "Erreur : /forumSocialX.jar n'existe pas."
+  exit 1
+fi
+
+# Attendre que la base de données soit prête
 /scripts/wait-for-it.sh db:5432 -t 30 -- echo "Database is up!"
 
-# Démarrer l'application Spring Boot (en supposant que le fichier JAR se trouve dans le dossier target)
+# Démarrer l'application
 java -jar /forumSocialX.jar
-ls -al /forumSocialX.jar
-# Attendre un moment pour voir si l'application se lance
-sleep 10
-
-# Vérifier si l'application fonctionne
-ps aux | grep 'java'  # Afficher les processus java en cours d'exécution
