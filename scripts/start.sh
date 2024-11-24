bf8185bd540c674daf023a54fcaec7fda8d25495
takoua1 <samaali.takoua@gmail.com>
2024-11-24 18:59:52 +0100

    Modified start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ if [ ! -f "/forumSocialX.jar" ]; then
 fi
 
 # Attendre que la base de données soit prête
-/scripts/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up!"
+/scripts/wait-for-it.sh db:5433 --timeout=60 --strict -- echo "Database is up!"
 
 # Lancer l'application Spring Boot
 java -Dserver.port=${PORT} -jar /forumSocialX.jar
